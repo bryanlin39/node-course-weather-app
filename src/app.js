35e04bae0e1488f4c1aaad514790cb35aca8480d
Bryan Lin <bryanlin39@gmail.com>
2022-03-20 14:57:32 -0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Set up static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Bryan'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Bryan'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Bryan',
+        helpMessage: 'This is the help message.'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'Please enter a valid location.'
+        });
+    }
+
+    geocode(req.query.search, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({error});
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error});
+            }
+
+            res.send({
+                location,
+                forecast: forecastData,
+                search: req.query.search
+            });
+        });
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Bryan',
+        error: 'Help article not found.'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Bryan',
+        error: 'Page not found.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=f2893b26b5cd5401c2315057d692e425&query=${latitude},${longitude}&units=f`;
+
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined);
+        } else if (body.error) {
+            callback('Unable to find location. Please try a different search term.', undefined);
+        } else {
+            callback(undefined, `The weather is currently ${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees F outside, and it feels like ${body.current.feelslike} degrees F.`);
+        }
+    });
+};
+
+module.exports = forecast;
